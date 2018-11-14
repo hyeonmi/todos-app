0e327b59738d2a822a85a100d3f8716fc4c40ab5
김현미 <kim.hyeonmi@navercorp.com>
2018-11-14 18:42:24 +0900

    add action unit test with redux-mock-store

--- src/actions/__tests__/index.test.ts
+++ src/actions/__tests__/index.test.ts
@@ -1,6 +1,8 @@
+import configureStore from 'redux-mock-store'
 import * as Actions from '../'
+import {initialState} from '../../types'
 
-describe('Action >', () =>{
+describe('Action Test 1 >', () =>{
     test('ADD_TODO 액션을 호출하면 payload에 할일 데이터가 반환 된다.', () => {
         // given
         const text = "blabla"
@@ -37,5 +39,56 @@ describe('Action >', () =>{
 
         // then
         expect(returnAction).toEqual(expectAction)
+    })
+})
+describe('Action Test 2 > ', () => {
+    let store :any
+    beforeEach(() => {
+        const mockStore = configureStore([])
+        store = mockStore(initialState)
+    })
+
+    afterEach(() => {
+        store = null
+    })
+
+    test('ADD_TODO 액션을 호출하면 payload에 할일 데이터가 반환 된다.', () => {
+        // given
+        const text = "blabla"
+
+        // when
+        store.dispatch(Actions.addTodo(text))
+
+        // then
+        const actions = store.getActions()
+        const expectAction = {
+            type: Actions.ActionTypes.ADD_TODO,
+            payload: {
+                todo: {
+                    text,
+                    id: 2,
+                    done: false
+                }
+            }
+        }
+        expect(actions).toEqual([expectAction])
+    })
+
+    test('TOGGLE_TODO 액션을 호출 히면 payload에 인자로 전달한 아이디가 반환 된다.', () => {
+        // given
+        const todoId = 1
+
+        // when
+        store.dispatch(Actions.toggleTodo(todoId))
+
+        // then
+        const actions = store.getActions()
+        const expectAction = {
+            type: Actions.ActionTypes.TOGGLE_TODO,
+            payload: {
+                todoId
+            }
+        }
+        expect(actions).toEqual([expectAction])
     })
 })
