b4715fb2126627c448b285e4ad4f0b72f3a4281a
hyeonmi <>
2018-11-17 18:26:14 +0900

    add TodoInputContainer test

---
+++ src/containers/__tests__/TodoInputContainer.test.tsx
@@ -0,0 +1,44 @@
+import * as React from 'react'
+import reduxMockStore from 'redux-mock-store'
+import {shallow} from 'enzyme'
+import TodoInputContainer from '../TodoInputContainer'
+import {initialState} from "../../types";
+import {ActionTypes} from "../../actions";
+
+describe('TodoInputContainer Component > ', () => {
+    let store :any
+    let wrapper : any
+    beforeEach(() => {
+        const mockStore = reduxMockStore([])
+        store = mockStore(initialState)
+        store.clearActions()
+        store.dispatch = jest.fn()
+        wrapper = shallow(<TodoInputContainer store={store}/>)
+    })
+
+    afterEach(() => {
+        store = null
+        wrapper
+    })
+
+    test('addTodo 디스페처가 프롭스로 매핑 되어야 한다.', () => {
+
+        // then
+        expect(wrapper.props()).toEqual(expect.objectContaining({
+            addTodo: expect.any(Function)
+        }))
+    })
+
+    test('addTodo 디스페처가 호출 되면 ADD_TODO 타입이 호출 되어야 한다', () => {
+
+        // when
+        const text = 'blabla'
+        wrapper.props().addTodo(text)
+
+        // then
+        expect(store.dispatch).toHaveBeenCalledWith({
+            type: ActionTypes.ADD_TODO,
+            payload: { todo: { text, done: false, id: 1}}
+        })
+    })
+})
